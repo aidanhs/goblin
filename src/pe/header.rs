0b0a5b84ef40b53826a44f192e8a25aa0e58fd2c
Aidan Hobson Sayers <aidanhs@cantab.net>
2017-11-18 11:58:51 +0000

    Signature isn't part of the coff header

--- src/pe/header.rs
+++ src/pe/header.rs
@@ -29,8 +29,6 @@ impl DosHeader {
 #[repr(C)]
 #[derive(Debug, PartialEq, Copy, Clone, Default)]
 pub struct CoffHeader {
-    /// COFF Magic: PE\0\0, little endian
-    pub signature: u32,
     /// The machine type
     pub machine: u16,
     pub number_of_sections: u16,
@@ -43,14 +41,13 @@ pub struct CoffHeader {
 
 pub const SIZEOF_COFF_HEADER: usize = 24;
 /// PE\0\0, little endian
-pub const COFF_MAGIC: u32 = 0x00004550;
+pub const PE_MAGIC: u32 = 0x00004550;
 pub const COFF_MACHINE_X86: u16 = 0x14c;
 pub const COFF_MACHINE_X86_64: u16 = 0x8664;
 
 impl CoffHeader {
     pub fn parse(bytes: &[u8], offset: &mut usize) -> error::Result<Self> {
         let mut coff = CoffHeader::default();
-        coff.signature = bytes.gread_with(offset, scroll::LE)?;
         coff.machine = bytes.gread_with(offset, scroll::LE)?;
         coff.number_of_sections = bytes.gread_with(offset, scroll::LE)?;
         coff.time_date_stamp = bytes.gread_with(offset, scroll::LE)?;
@@ -66,6 +63,8 @@ impl CoffHeader {
 #[derive(Debug, PartialEq, Copy, Clone, Default)]
 pub struct Header {
     pub dos_header: DosHeader,
+    /// PE Magic: PE\0\0, little endian
+    pub signature: u32,
     pub coff_header: CoffHeader,
     pub optional_header: Option<optional_header::OptionalHeader>,
 }
@@ -74,19 +73,20 @@ impl Header {
     pub fn parse(bytes: &[u8]) -> error::Result<Self> {
         let dos_header = DosHeader::parse(&bytes)?;
         let mut offset = dos_header.pe_pointer as usize;
+        let signature = bytes.gread_with(&mut offset, scroll::LE)?;
         let coff_header = CoffHeader::parse(&bytes, &mut offset)?;
         let optional_header =
             if coff_header.size_of_optional_header > 0 {
                 Some (bytes.pread::<optional_header::OptionalHeader>(offset)?)
             }
         else { None };
-        Ok( Header { dos_header: dos_header, coff_header: coff_header, optional_header: optional_header })
+        Ok( Header { dos_header: dos_header, signature: signature, coff_header: coff_header, optional_header: optional_header })
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{DOS_MAGIC, COFF_MAGIC, COFF_MACHINE_X86, Header};
+    use super::{DOS_MAGIC, PE_MAGIC, COFF_MACHINE_X86, Header};
 
     const CRSS_HEADER: [u8; 688] =
         [0x4d, 0x5a, 0x90, 0x00, 0x03, 0x00, 0x00, 0x00, 0x04, 0x00, 0x00, 0x00, 0xff, 0xff, 0x00, 0x00,
@@ -137,7 +137,7 @@ mod tests {
     fn crss_header () {
         let header = Header::parse(&&CRSS_HEADER[..]).unwrap();
         assert!(header.dos_header.signature == DOS_MAGIC);
-        assert!(header.coff_header.signature == COFF_MAGIC);
+        assert!(header.signature == PE_MAGIC);
         assert!(header.coff_header.machine == COFF_MACHINE_X86);
         println!("header: {:?}", &header);
     }
